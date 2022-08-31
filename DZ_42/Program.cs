7acf61e7b8eb43855e0b93b2fe5642b6d5e0f46c
NastyaM <Nastya@mail.com>
2022-08-31 09:15:23 +0700

    DZ#42

---
+++ DZ_42/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int number = Convert.ToInt32(Console.ReadLine());
+string binary = Convert.ToString(number, 2);
+Console.WriteLine(binary);
