7c629eb9984d0d635c929b4473136a4479b6ef2f
NastyaM <Nastya@mail.com>
2022-08-31 09:10:51 +0700

    DZ#41

---
+++ DZ_41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// // 0, 7, 8, -2, -2 -> 2
+// // 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите массив из пяти чисел: ");
+int[] testArray = new int[5];
+int count = 0;
+for (int i = 0; i < 5; i++)
+{
+testArray[i] = Convert.ToInt32(Console.ReadLine());
+if (testArray[i] > 0) count++;
+}
+Console.WriteLine($"[{String.Join("; ", testArray)}]");
+Console.WriteLine(count); 
